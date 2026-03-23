5d81012ea89f4d3627a04082ada4ffa3e83b2eef
Steve Bunnage <sbunnage@gmail.com>
2026-03-23 10:24:34 +0000

    Update __init__.py

--- custom_components/vendor_overrides/__init__.py
+++ custom_components/vendor_overrides/__init__.py
@@ -4,17 +4,17 @@ from homeassistant.helpers import service
 from .const import DOMAIN
 from .storage import VendorOverrideStorage
 from .api import VendorOverrideAPI
-#from .websocket import async_register_websocket
+# from .websocket import async_register_websocket
 
 async def async_setup(hass: HomeAssistant, config: ConfigType):
     storage = VendorOverrideStorage(hass)
-    #   await storage.async_load()
+    await storage.async_load()
 
     # Register REST API
     hass.http.register_view(VendorOverrideAPI(storage))
 
     # Register WebSocket API
-    await async_register_websocket(hass, storage)
+    # await async_register_websocket(hass, storage)
 
     # Register service
     async def handle_set_override(call):
